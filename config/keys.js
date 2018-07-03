0e8c73efcd55610d4ccd4192b52489dd598bd47f
AMG <alexgood1@gmail.com>
2018-07-03 04:41:40 -0700

    create user login and password auth

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://Alexandra:crarrm11@ds141185.mlab.com:41185/devcompany"
+  mongoURI: "mongodb://Alexandra:crarrm11@ds141185.mlab.com:41185/devcompany",
+  secretOrKey: "secret"
 };
---
+++ validation/is-empty.js
@@ -0,0 +1,9 @@
+// specialized isEmpty func to check for undef, empty, null and a string
+
+const isEmpty = value =>
+  value === undefined ||
+  value === null ||
+  (typeof value === "object" && Object.keys(value).length === 0) ||
+  (typeof value === "string" && value.trim().length === 0);
+
+module.exports = isEmpty;
